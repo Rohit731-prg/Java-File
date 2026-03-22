053577b6e74b31b936ce3f1aab0f1d920006b200
Rohit731-prg <rohitsingha821380@gmail.com>
2026-03-22 21:43:31 +0530

    Loop programs added

--- Loops/File2.java
+++ Loops/File2.java
@@ -4,6 +4,16 @@ public class File2 {
     public static void main(String[] arg) {
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+        while (num > 0) {
+            int reminder = num % 10;
+            sum = reminder + (sum * 10);
+            num /= 10;
+        }
+
+        System.out.println("\nReverce Number: " + sum);
+        sc.close();
     }
 }
